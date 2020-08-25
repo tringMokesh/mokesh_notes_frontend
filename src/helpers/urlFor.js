61c4e4b1734e9245b227c5574f0b7194c0a23194
Mokesh-mock <mokeshchitti@gmail.com>
2020-08-25 16:56:48 +0530

    backend url configured

--- src/helpers/urlFor.js
+++ src/helpers/urlFor.js
@@ -1,5 +1,5 @@
 const urlFor = (endpoint) => {
- return `http://localhost:3000/${endpoint}`;
+ return `http://ec2-13-127-224-143.ap-south-1.compute.amazonaws.com:3000/${endpoint}`;
 };
 
 export default urlFor;
